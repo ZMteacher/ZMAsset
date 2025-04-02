3578286200fd57493c237e7f66353c8c9f09e2c9
zm <zhumengxyedu@163.com>
2025-04-02 22:23:33 +0800

    同步最新框架

---
+++ ZMAssest/Assets/ZMPackages/ZMAsset/Runtime/BundleLoad/IAddressableAssetInterface.cs
@@ -0,0 +1,11 @@
+﻿using Cysharp.Threading.Tasks;
+
+namespace ZM.ZMAsset
+{
+    public interface IAddressableAssetInterface
+    {
+        UniTask<AssetsRequest> InstantiateAsyncFormPoolAas(string path, BundleModuleEnum bundleModule, object param1 = null, object param2 = null, object param3 = null);
+
+        UniTask<T> LoadResourceAsyncAas<T>(string fullPath, BundleModuleEnum moduleEnum) where T : UnityEngine.Object;
+    }
+}
